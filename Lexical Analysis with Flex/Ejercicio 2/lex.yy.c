5adb443396ce773c2692490adf801913791fff51
Dankof04 <naninaniel04@gmail.com>
2025-09-12 12:18:52 +0200

    progreso ejercicio 2

--- Lexical Analysis with Flex/Ejercicio 2/lex.yy.c
+++ Lexical Analysis with Flex/Ejercicio 2/lex.yy.c
@@ -360,23 +360,23 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[11] =
+static const flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    4,    3,    2,    3,    0,    0,    1,    0
+        0,    0,    4,    2,    2,    1,    0,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
+        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        3,    1,    4,    3,    1,    1,    1,    1,    1,    1,
 
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,31 +397,29 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[5] =
+static const YY_CHAR yy_meta[4] =
     {   0,
-        1,    1,    1,    1
+        1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[11] =
+static const flex_int16_t yy_base[10] =
     {   0,
-        0,    0,    8,    9,    3,    0,    3,    0,    9,    9
+        0,    0,    6,    7,    2,    0,    0,    0,    7
     } ;
 
-static const flex_int16_t yy_def[11] =
+static const flex_int16_t yy_def[10] =
     {   0,
-       10,    1,   10,   10,   10,    5,    5,    5,   10,    0
+        9,    1,    9,    9,    9,    5,    5,    5,    0
     } ;
 
-static const flex_int16_t yy_nxt[14] =
+static const flex_int16_t yy_nxt[11] =
     {   0,
-        4,    5,    6,    4,    7,    8,    9,   10,    3,   10,
-       10,   10,   10
+        4,    5,    6,    7,    8,    9,    3,    9,    9,    9
     } ;
 
-static const flex_int16_t yy_chk[14] =
+static const flex_int16_t yy_chk[11] =
     {   0,
-        1,    1,    1,    1,    5,    5,    7,    3,   10,   10,
-       10,   10,   10
+        1,    1,    1,    5,    5,    3,    9,    9,    9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -441,7 +439,7 @@ char *yytext;
 #line 1 "ejercicio2.l"
 #line 3 "ejercicio2.l"
  int contador=0;
-#line 445 "lex.yy.c"
+#line 443 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -661,7 +659,7 @@ YY_DECL
 #line 5 "ejercicio2.l"
 
 
-#line 665 "lex.yy.c"
+#line 663 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -688,13 +686,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 11 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 9 );
+		while ( yy_base[yy_current_state] != 7 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -722,20 +720,20 @@ case 1:
 /* rule 1 can match eol */
 YY_RULE_SETUP
 #line 7 "ejercicio2.l"
-;
+contador++;
 	YY_BREAK
 case 2:
 /* rule 2 can match eol */
 YY_RULE_SETUP
-#line 8 "ejercicio2.l"
-contador++;
+#line 10 "ejercicio2.l"
+;
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 11 "ejercicio2.l"
+#line 12 "ejercicio2.l"
 ECHO;
 	YY_BREAK
-#line 739 "lex.yy.c"
+#line 737 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1032,7 +1030,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 11 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1060,11 +1058,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 11 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 10);
+	yy_is_jam = (yy_current_state == 9);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1740,7 +1738,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 11 "ejercicio2.l"
+#line 12 "ejercicio2.l"
 
 
 int main(){
