423541c282ac13a7b4a67397e923f6d751369d85
Dankof04 <naninaniel04@gmail.com>
2025-09-12 13:00:48 +0200

    ej2

--- Lexical Analysis with Flex/Ejercicio 2/lex.yy.c
+++ Lexical Analysis with Flex/Ejercicio 2/lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 4
+#define YY_END_OF_BUFFER 5
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,9 +360,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[12] =
     {   0,
-        0,    0,    4,    2,    2,    1,    0,    1,    0
+        0,    0,    5,    3,    3,    1,    0,    2,    0,    1,
+        0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -402,24 +403,28 @@ static const YY_CHAR yy_meta[4] =
         1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[10] =
+static const flex_int16_t yy_base[12] =
     {   0,
-        0,    0,    6,    7,    2,    0,    0,    0,    7
+        0,    0,    5,    3,    5,    0,    0,    0,    0,    9,
+        9
     } ;
 
-static const flex_int16_t yy_def[10] =
+static const flex_int16_t yy_def[12] =
     {   0,
-        9,    1,    9,    9,    9,    5,    5,    5,    0
+       11,    1,   11,   11,   11,    4,    4,    4,    5,   11,
+        0
     } ;
 
-static const flex_int16_t yy_nxt[11] =
+static const flex_int16_t yy_nxt[13] =
     {   0,
-        4,    5,    6,    7,    8,    9,    3,    9,    9,    9
+        4,    5,    6,    7,   11,    8,    9,   10,    3,   11,
+       11,   11
     } ;
 
-static const flex_int16_t yy_chk[11] =
+static const flex_int16_t yy_chk[13] =
     {   0,
-        1,    1,    1,    5,    5,    3,    9,    9,    9,    9
+        1,    1,    1,    4,    3,    4,    5,    5,   11,   11,
+       11,   11
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -439,7 +444,7 @@ char *yytext;
 #line 1 "ejercicio2.l"
 #line 3 "ejercicio2.l"
  int contador=0;
-#line 443 "lex.yy.c"
+#line 448 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -659,7 +664,7 @@ YY_DECL
 #line 5 "ejercicio2.l"
 
 
-#line 663 "lex.yy.c"
+#line 668 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -686,13 +691,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 12 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 9 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -725,15 +730,21 @@ contador++;
 case 2:
 /* rule 2 can match eol */
 YY_RULE_SETUP
-#line 10 "ejercicio2.l"
+#line 8 "ejercicio2.l"
 ;
 	YY_BREAK
 case 3:
+/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 12 "ejercicio2.l"
+#line 11 "ejercicio2.l"
+;
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 13 "ejercicio2.l"
 ECHO;
 	YY_BREAK
-#line 737 "lex.yy.c"
+#line 748 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1030,7 +1041,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 12 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1058,11 +1069,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 12 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 11);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1738,7 +1749,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 12 "ejercicio2.l"
+#line 13 "ejercicio2.l"
 
 
 int main(){
